c342ccf0e90c3d3c2c4be5f41716f177fb86ec4f
Lucas Ferreira de Almeida <lucasf.almeida.dev@gmail.com>
2023-09-30 16:14:47 -0300

    fix: delete main.py file

--- main.py
+++
@@ -1,6 +0,0 @@
-def sum(a: int, b: int) -> int:
-    return a + b
-
-num1 = int(input('Digite o primeiro número: '))
-num2 = int(input('Digite o segundo número: '))
-print(f'Soma: {sum(num1, num2)}')
